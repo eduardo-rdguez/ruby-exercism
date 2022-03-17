1b837a9a4cc58f19a29ec653ea59f3ad07fc602f
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-17 17:52:42 -0600

    Add booleans exercise solution

---
+++ lib/booleans/attendee.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Attendee exercise
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  # rubocop:disable Naming/PredicateName
+  def has_pass?
+    @pass_id
+  end
+  # rubocop:enable Naming/PredicateName
+
+  def fits_ride?(ride_minimum_height)
+    @height >= ride_minimum_height
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    fits_ride?(ride_minimum_height) && has_pass?
+  end
+end
