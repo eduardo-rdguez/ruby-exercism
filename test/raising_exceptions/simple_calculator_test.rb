153efde627bed7e13027ee69add59790d5a4aa4b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-17 16:13:15 -0600

    Add the solution to the simple calculator exercise

---
+++ test/raising_exceptions/simple_calculator_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require './lib/raising_exceptions/simple_calculator.rb'
+
+# Test simple calculator
+class SimpleCalculatorTest < Minitest::Test
+  def test_addition_with
+    assert_equal '22 + 25 = 47', SimpleCalculator.calculate(22, 25, '+')
+  end
+
+  def test_multiplication
+    assert_equal '3 * 21 = 63', SimpleCalculator.calculate(3, 21, '*')
+  end
+
+  def test_division
+    assert_equal '3 * 21 = 63', SimpleCalculator.calculate(3, 21, '*')
+  end
+
+  def test_division
+    assert_equal '72 / 9 = 8', SimpleCalculator.calculate(72, 9, '/')
+  end
+
+  def test_rescues_division_by_zero_exception
+    assert_equal 'Division by zero is not allowed.', SimpleCalculator.calculate(33, 0, '/')
+  end
+
+  def test_no_number_first_operand
+    assert_raises(ArgumentError) {
+      SimpleCalculator.calculate('1', 2, '+')
+    }
+  end
+
+  def test_no_number_second_operand
+    assert_raises(ArgumentError) {
+      SimpleCalculator.calculate(1, '2', '+')
+    }
+  end
+
+  def test_raises_exception_for_non_valid_operations
+    assert_raises(SimpleCalculator::UnsupportedOperation) {
+      SimpleCalculator.calculate(1, 2, '**')
+    }
+  end
+
+  def test_raises_exception_when_operation_is_nil
+    assert_raises(SimpleCalculator::UnsupportedOperation) {
+      SimpleCalculator.calculate(1, 2, nil)
+    }
+  end
+
+  def test_raises_exception_when_operation_is_an_empty_string
+    assert_raises(SimpleCalculator::UnsupportedOperation) {
+      SimpleCalculator.calculate(1, 2, '')
+    }
+  end
+end
