11a88128e9521e14aab7d93e4d4cb43868e1de26
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-17 16:39:20 -0600

    Refactor the solution of the simple calculator exercise

--- lib/raising_exceptions/simple_calculator.rb
+++ lib/raising_exceptions/simple_calculator.rb
@@ -4,67 +4,33 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
   UNSUPPORTED_OPERATION_ERROR_MESSAGE = 'UnsupportedOperation'
-  ARGUMENT_ERROR_MESSAGE = 'Argument is not Numeric'
-  DIVISION_BY_ZERO_ERROR_MESSAGE = 'Division by zero is not allowed.'
+  ZERO_DIVISION_ERROR_MESSAGE = 'Division by zero is not allowed.'
 
   class UnsupportedOperation < StandardError
+    UNSUPPORTED_OPERATION_ERROR_MESSAGE
   end
 
   def self.calculate(first_operand, second_operand, operation)
-    self.validate_operands(first_operand, second_operand)
-    if self.validate_operation?(operation)
-      result = evaluate_operation(first_operand, second_operand, operation)
-      return self.message(first_operand, second_operand, operation, result)
-    else
-      raise UnsupportedOperation.new(UNSUPPORTED_OPERATION_ERROR_MESSAGE)
-    end
-  end
+    raise UnsupportedOperation unless self.validate_operation?(operation)
+    raise ArgumentError unless self.validate_operands?(first_operand, second_operand)
 
-  def self.validate_operands(first_operand, second_operand)
-    no_number_first_operand = !first_operand.is_a?(Numeric)
-    no_number_second_operand = !second_operand.is_a?(Numeric)
-
-    if (no_number_first_operand || no_number_second_operand)
-      raise ArgumentError.new(ARGUMENT_ERROR_MESSAGE)
-    end
+    evaluate_operation(first_operand, second_operand, operation)
   end
 
   def self.validate_operation?(operation)
     ALLOWED_OPERATIONS.include?(operation)
   end
 
+  def self.validate_operands?(first_operand, second_operand)
+    first_operand.is_a?(Numeric) && second_operand.is_a?(Numeric)
+  end
+
   def self.evaluate_operation(first_operand, second_operand, operation)
-    case operation
-      when ALLOWED_OPERATIONS[0]
-        self.add_operation(first_operand, second_operand)
-      when ALLOWED_OPERATIONS[1]
-        self.division_operation(first_operand, second_operand)
-      when ALLOWED_OPERATIONS[2]
-        self.multiply_operation(first_operand, second_operand)
-    end
-  end
-
-  def self.add_operation(first_operand, second_operand)
-    first_operand + second_operand
-  end
-
-  def self.multiply_operation(first_operand, second_operand)
-    first_operand * second_operand
-  end
-
-  def self.division_operation(first_operand, second_operand)
     begin
-      first_operand / second_operand
+      result = first_operand.public_send(operation, second_operand)
+      return "#{first_operand} #{operation} #{second_operand} = #{result}"
     rescue
-      DIVISION_BY_ZERO_ERROR_MESSAGE
-    end
-  end 
-
-  def self.message(first_operand, second_operand, operation, result)
-    if result.is_a?(Numeric)
-      "#{first_operand} #{operation} #{second_operand} = #{result}"
-    else
-      result
+      return ZERO_DIVISION_ERROR_MESSAGE
     end
   end
 end
