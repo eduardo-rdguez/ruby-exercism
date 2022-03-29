edc7cdbb7a88b966cf34e0a56d06a78702304a54
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-28 22:46:38 -0600

    Add word count exercise solution

---
+++ lib/enum_methods/word_count.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Word count exercise
+class Phrase
+  WORD_REGEX = /\w+'\w+|\w+/
+
+  attr_reader :phrase
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def word_count
+    phrase
+      .scan(WORD_REGEX)
+      .map(&:downcase)
+      .tally
+  end
+end
