f60296257e1eaaff34e9771af1a9c05f3334575a
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-29 22:52:24 -0600

    Update hamming exercise solution

--- lib/raising_exceptions/hamming.rb
+++ lib/raising_exceptions/hamming.rb
@@ -5,11 +5,11 @@ class Hamming
   def self.compute(strand_one, strand_two)
     raise ArgumentError unless strand_one.length == strand_two.length
 
-    differences(strand_one, strand_two).count
+    differences(strand_one.chars, strand_two.chars).count
   end
 
   def self.differences(strand_one, strand_two)
-    strand_one.chars.zip(strand_two.chars).reject { |a, b| a == b }
+    strand_one.zip(strand_two).reject { |a, b| a == b }
   end
 
   private_class_method :differences
